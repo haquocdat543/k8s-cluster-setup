35871044bcb5c06ad625aceabb1865c0e03e2e84
haquocdat543 <wwwdatha543@gmail.com>
2023-11-14 12:45:22 +0000

    Update main.tf
    Fix output.

--- cloud/terraform-eks/main.tf
+++ cloud/terraform-eks/main.tf
@@ -108,9 +108,9 @@ resource "aws_eks_cluster" "eks" {
 #Output
 
 output "endpoint" {
-  value = aws_eks_cluster.example.endpoint
+  value = aws_eks_cluster.eks.endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.example.certificate_authority[0].data
+  value = aws_eks_cluster.eks.certificate_authority[0].data
 }

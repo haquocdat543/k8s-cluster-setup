2a166a35a893c15c4a3830b200839022d063c85f
haquocdat543 <wwwdatha543@gmail.com>
2023-11-14 12:47:31 +0000

    Update main.tf
    Fix subnet.

--- cloud/terraform-eks/main.tf
+++ cloud/terraform-eks/main.tf
@@ -44,7 +44,7 @@ resource "aws_subnet" "public1" {
 }
 resource "aws_subnet" "public2" {
   vpc_id            = aws_vpc.prod-vpc.id
-  cidr_block        = "10.0.0.0/24"
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-1c"
 
   tags = {

86c40ef3e83a455f6a9097adb902f454bdfdce8d
haquocdat543 <wwwdatha543@gmail.com>
2023-11-14 13:28:04 +0000

    Update main.tf
    public1.
    public2.
    map_public_ip_on_launch = true

--- cloud/terraform-eks/main.tf
+++ cloud/terraform-eks/main.tf
@@ -37,6 +37,7 @@ resource "aws_subnet" "public1" {
   vpc_id            = aws_vpc.prod-vpc.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public1"
@@ -46,6 +47,7 @@ resource "aws_subnet" "public2" {
   vpc_id            = aws_vpc.prod-vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-1c"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public2"

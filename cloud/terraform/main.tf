459a314325db4a6f54212c41d9506e9f91781b13
haquocdat543 <wwwdatha543@gmail.com>
2023-11-14 07:00:39 +0000

    Update main.tf
    Modified security_group

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -54,32 +54,12 @@ resource "aws_security_group" "ProdSecurityGroup" {
   vpc_id      = aws_vpc.prod-vpc.id
 
   ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    description = "ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "8080"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {

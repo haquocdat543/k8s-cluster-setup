7d6c2ace5434cc354ac47cf7b543a3bd886ca17a
haquocdat543 <wwwdatha543@gmail.com>
2024-04-19 21:33:10 +0700

    Add label to terraform eks node group

--- cloud/terraform-eks/eks_node_group.tf
+++ cloud/terraform-eks/eks_node_group.tf
@@ -2,17 +2,25 @@ resource "aws_eks_node_group" "node-group-1" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "Medium-Group"
   node_role_arn   = aws_iam_role.node-group.arn
-  subnet_ids      = [aws_subnet.first-subnet.id, aws_subnet.second-subnet.id]
   instance_types  = ["t3.medium"]
+
+  subnet_ids      = [
+    aws_subnet.first-subnet.id,
+    aws_subnet.second-subnet.id
+  ]
 
   scaling_config {
     desired_size = 1
     max_size     = 2
-    min_size     = 1
+    min_size     = 0
   }
 
   update_config {
     max_unavailable = 1
+  }
+
+  labels = {
+    role = "general-purpose"
   }
 
   depends_on = [
@@ -26,17 +34,25 @@ resource "aws_eks_node_group" "node-group-2" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "High-Group"
   node_role_arn   = aws_iam_role.node-group.arn
-  subnet_ids      = [aws_subnet.first-subnet.id, aws_subnet.second-subnet.id]
   instance_types  = ["t3.2xlarge"]
+
+  subnet_ids      = [
+    aws_subnet.first-subnet.id,
+    aws_subnet.second-subnet.id
+  ]
 
   scaling_config {
     desired_size = 1
     max_size     = 2
-    min_size     = 1
+    min_size     = 0
   }
 
   update_config {
     max_unavailable = 1
+  }
+
+  labels = {
+    role = "special-purpose"
   }
 
   depends_on = [
